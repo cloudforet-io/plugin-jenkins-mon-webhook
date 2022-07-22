660ecc9eb5c1dbb9260aad7996a23fde8ea29f00
jihyungsong <bluese05@gmail.com>
2022-07-22 14:05:42 +0900

    feat: build event

--- test/api/test_event_api.py
+++ test/api/test_event_api.py
@@ -12,41 +12,8 @@ class TestEvent(TestCase):
 
     def test_parse(self):
         params = {
-            "options": {
-
-            },
-            "data": {
-                "to": "abc@localhost.net",
-		        "event": {
-                    "name": "Load average is too high (per CPU load over 1.5 for 5m)",
-                    "recovery_id": "{EVENT.RECOVERY.ID}",
-                    "id": "7467",
-                    "severity": "Average",
-                    "recovery_name": "{EVENT.RECOVERY.NAME}",
-                    "status": "PROBLEM"
-                },
-                "message": "Problem started at 12:51:50 on 2021.08.27\r\nProblem name: Load average is too high (per CPU load over 1.5 for 5m)\r\nHost: zabbix-test\r\nSeverity: Average\r\nOperational data: Load averages(1m 5m 15m): (1.69 1.39 1.01), # of CPUs: 1\r\nOriginal problem ID: 7467\r\n",
-                "title": "Problem: Load average is too high (per CPU load over 1.5 for 5m)",
-                "item": {
-                    "key": "system.cpu.load[all,avg1]",
-                    "id": "42529",
-                    "value": "1.69"
-                },
-                "host": {
-                    "name": "zabbix-test",
-                    "description": "",
-                    "dns": "",
-                    "connection_info": "172.31.4.239",
-                    "id": "10490",
-                    "visible_name": "zabbix-test"
-                },
-                "trigger": {
-                    "severity": "Average",
-                    "name": "Load average is too high (per CPU load over 1.5 for 5m)",
-                    "status": "PROBLEM",
-                    "id": "21028"
-                }
-            }
+            'options': {},
+            'data': {"build": {"status": "SUCCESS", "log": "", "queue_id": 24.0, "url": "job/Freestyle%20Job%201/12/", "notes": "", "scm": {"culprits": [], "changes": []}, "phase": "FINALIZED", "artifacts": {}, "full_url": "http://13.124.148.140:8080/job/Freestyle%20Job%201/12/", "timestamp": 1658465683072.0, "duration": 182.0, "number": 12.0}, "name": "Freestyle Job 1", "display_name": "Freestyle Job 1", "url": "job/Freestyle%20Job%201/"}
         }
 
         test_cases = [params]
@@ -56,7 +23,6 @@ class TestEvent(TestCase):
             parsed_data = self.monitoring.Event.parse({'options': {}, 'data': test_case.get('data')})
             print_json(parsed_data)
             print()
-
 
 
 if __name__ == "__main__":
